f3d354c48517bffe4646ff42bc5cf91e2ca4bc2c
EZEQUIEL\ezequ <ezequiel_morales_1999@hotmail.com>
2025-02-19 20:19:08 -0300

    Added Map elements, Added Enemy Behavior.

--- BrackeysJamProject/Assets/Scripts/Enemy.cs
+++ BrackeysJamProject/Assets/Scripts/Enemy.cs
@@ -1,16 +1,68 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    float _minAttackDistance = 2f;
+
+    [SerializeField] NavMeshAgent _agent;
+
+    Vector3 _target;
+
+    enum State
+    {
+        Chasing,
+        Attacking,
+        Waiting,
+        Dead
+    }
+    [SerializeField] State _state;
+
+    IEnumerator _enumerator = null;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        switch (_state)
+        {
+            case State.Chasing:
+
+                _target = GameManager.Instance.PlayerGet.transform.position;
+                Move();
+
+                float distance = Vector3.Distance(transform.position, _target);
+
+                if (distance < _minAttackDistance)
+                {
+                    _state = State.Attacking;
+                }
+
+                break;
+
+            case State.Attacking:
+
+                Debug.Log("Player Attacked");
+                _state = State.Waiting;
+
+                break;
+
+            case State.Waiting:
+                break;
+
+            case State.Dead:
+                break;
+        }
+    }
+
+    void Move()
+    {
+        Vector3 offset = _target + (transform.position - _target).normalized;
+        _agent.SetDestination(offset);
     }
 }
--- BrackeysJamProject/Assets/Scripts/GameManager.cs
+++ BrackeysJamProject/Assets/Scripts/GameManager.cs
@@ -2,13 +2,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Player _player;
+
+    public Player PlayerGet { get { return _player; } }
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
