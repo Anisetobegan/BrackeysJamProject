765387172742a7c52f56762951b0feb481abf50b
Mati Morales <matias_morales_1996@hotmail.com>
2025-03-16 02:18:01 -0300

    Polished healthbar

--- BrackeysJamProject/Assets/Scripts/HealthBar.cs
+++ BrackeysJamProject/Assets/Scripts/HealthBar.cs
@@ -1,9 +1,17 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] protected Image bar;
+
+    [SerializeField] protected RectTransform heartIcon;
+
+    private void Start()
+    {
+        heartIcon.DOPunchScale(Vector3.one * 0.1f, 1f, 0).SetLoops(-1);
+    }
 
     public void HealthBarUpdate(float newPercentage)
     {
